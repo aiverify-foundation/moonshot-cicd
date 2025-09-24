3b001fbad253bfc03976b748f15dff1d1435d9fa
aivf_baisheng <kok_bai_sheng@imda.gov.sg>
2025-09-24 16:35:16 +0800

    moon-536 added  fixes for the sidebar to work properly

--- moonshot_portal_app/src/components/app-sidebar.tsx
+++ moonshot_portal_app/src/components/app-sidebar.tsx
@@ -17,27 +17,7 @@ const items = [
     title: "Home",
     url: "#",
     icon: Home,
-  },
-  {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
+  }
 ]
 
 export function AppSidebar() {
