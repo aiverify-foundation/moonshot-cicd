acc52a673ba508e5dd7431efeacafcdf53cbef7e
aivf_baisheng <kok_bai_sheng@imda.gov.sg>
2025-09-30 11:55:58 +0800

    moon-536 Removed unneeded files and changed configuration/ serving to use path.

--- moonshot_portal_app/app/layout.tsx
+++ moonshot_portal_app/app/layout.tsx
@@ -29,13 +29,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-            <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
-    </SidebarProvider>
+        <SidebarProvider>
+          <AppSidebar />
+          <main>
+            <SidebarTrigger />
+            {children}
+          </main>
+        </SidebarProvider>
       </body>
     </html>
   );
