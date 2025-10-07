9ebff06b8ae20a48daea9beab4555dd4dbb58d58
aivf_baisheng <kok_bai_sheng@imda.gov.sg>
2025-10-07 14:48:24 +0800

    moon-538 Add in the button that requires at least one bundle to be selected to go to the next page

---
+++ moonshot_portal_app/app/select_model/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { ArrowLeft } from 'lucide-react';
+import Link from 'next/link';
+
+export default function SelectModel() {
+  return (
+    <main className="p-8">
+    </main>
+  );
+}
